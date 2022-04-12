68b89d51b1dc84013f6bdd002f246b22cd8e329a
Kartikeya Pande <kpande479@gmail.com>
2022-04-12 12:16:31 -0400

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -139,3 +139,32 @@ resource "aws_s3_bucket" "logs" {
     yor_trace            = "ce72f84f-4cb6-4f67-b540-54d7e998df19"
   }
 }
+
+resource "aws_s3_bucket" "logs2" {
+  bucket = "${local.resource_prefix.value}-logs"
+  acl    = "log-delivery-write"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      }
+    }
+  }
+  force_destroy = true
+  tags = {
+    Name                 = "${local.resource_prefix.value}-logs"
+    Environment          = local.resource_prefix.value
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/aws/s3.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "try-bridgecrew"
+    git_repo             = "terragoat"
+    yor_trace            = "ce72f84f-4cb6-4f67-b540-54d7e998df19"
+  }
+}
