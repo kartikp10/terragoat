5c85c5903b712dce95926c67b1954cf0fd8e8829
Kartikeya Pande <kpande479@gmail.com>
2022-04-26 13:06:21 -0400

    Create s3_new.tf

--- terraform/aws/s3_new.tf
+++ terraform/aws/s3_new.tf
@@ -4,17 +4,6 @@ resource "aws_s3_bucket" "logs" {
 	# checkov:skip=CKV2_AWS_6: ADD REASON
   bucket = "${local.resource_prefix.value}-logs"
   acl    = "log-delivery-write"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
-      }
-    }
-  }
   force_destroy = true
   tags = {
     Name                 = "${local.resource_prefix.value}-logs"
